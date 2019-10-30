7f175c1124807d714fa2709c426b72759fcacdbf
Melvillian <melvillevt@gmail.com>
2019-10-30 16:46:05 -0400

    midway through figuring out how to access table data. Now going to try chrome_headless's example to see how it works

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use headless_chrome::{Browser, protocol::page::ScreenshotFormat};
-use failure::Error;
+use headless_chrome::Browser;
 
 fn browse_wikipedia() -> Result<(), failure::Error> {
     let browser = Browser::default()?;
@@ -7,29 +6,35 @@ fn browse_wikipedia() -> Result<(), failure::Error> {
     let tab = browser.wait_for_initial_tab()?;
 
     /// Navigate to wikipedia
-    tab.navigate_to("https://www.wikipedia.org")?;
+    tab.navigate_to("https://money.cnn.com/data/afterhours/")?;
 
+    tab.wait_for_element("div#wsod_marketMoversContainer")?;
+
+    let node = tab.find_element("div#wsod_marketMoversContainer")?;
+
+
+    println!("{:?}", node.get_description()?.find(|n| n.node_name == "tr"));
     /// Wait for network/javascript/dom to make the search-box available
     /// and click it.
-    tab.wait_for_element("input#searchInput")?.click()?;
-
-    /// Type in a query and press `Enter`
-    tab.type_str("WebKit")?.press_key("Enter")?;
-
-    /// We should end up on the WebKit-page once navigated
-    tab.wait_for_element("#firstHeading")?;
-    assert!(tab.get_url().ends_with("WebKit"));
-
-    /// Take a screenshot of the entire browser window
-    let _jpeg_data = tab.capture_screenshot(
-                        ScreenshotFormat::JPEG(Some(75)),
-                        None,
-                        true)?;
+    // tab.wait_for_element("input#searchInput")?.click()?;
+    //
+    // /// Type in a query and press `Enter`
+    // tab.type_str("WebKit")?.press_key("Enter")?;
+    //
+    // /// We should end up on the WebKit-page once navigated
+    // tab.wait_for_element("#firstHeading")?;
+    // assert!(tab.get_url().ends_with("WebKit"));
+    //
+    // /// Take a screenshot of the entire browser window
+    // let _jpeg_data = tab.capture_screenshot(
+    //                     ScreenshotFormat::JPEG(Some(75)),
+    //                     None,
+    //                     true)?;
 
     /// Take a screenshot of just the WebKit-Infobox
-    let _png_data = tab
-        .wait_for_element("#mw-content-text > div > table.infobox.vevent")?
-        .capture_screenshot(ScreenshotFormat::PNG)?;
+    // let _png_data = tab
+    //     .wait_for_element("#mw-content-text > div > table.infobox.vevent")?
+    //     .capture_screenshot(ScreenshotFormat::PNG)?;
     Ok(())
 }
 
