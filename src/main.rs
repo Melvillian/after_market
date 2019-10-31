673e90533f2722174b9b78e527e6f1aa32687780
Melvillian <melvillevt@gmail.com>
2019-10-30 21:37:10 -0400

    partway done, in borrow hell

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,39 @@
 use headless_chrome::Browser;
+use std::default::Default;
 
 fn browse_wikipedia() -> Result<(), failure::Error> {
     let browser = Browser::default()?;
 
     let tab = browser.wait_for_initial_tab()?;
 
-    /// Navigate to wikipedia
+    // navigate to the afterhours info webpage on CNN
     tab.navigate_to("https://money.cnn.com/data/afterhours/")?;
 
+    // locate the HTML table with the afterhours trading Gainers and Losers
     tab.wait_for_element("div#wsod_marketMoversContainer")?;
-
     let node = tab.find_element("div#wsod_marketMoversContainer")?;
+    let description = node.get_description()?;
+    let table = description.find(|n| n.node_name == "TBODY").unwrap();
 
+    let rows = table.children.as_ref().unwrap();
 
-    println!("{:?}", node.get_description()?.find(|n| n.node_name == "tr"));
-    /// Wait for network/javascript/dom to make the search-box available
-    /// and click it.
-    // tab.wait_for_element("input#searchInput")?.click()?;
-    //
-    // /// Type in a query and press `Enter`
-    // tab.type_str("WebKit")?.press_key("Enter")?;
-    //
-    // /// We should end up on the WebKit-page once navigated
-    // tab.wait_for_element("#firstHeading")?;
-    // assert!(tab.get_url().ends_with("WebKit"));
-    //
-    // /// Take a screenshot of the entire browser window
-    // let _jpeg_data = tab.capture_screenshot(
-    //                     ScreenshotFormat::JPEG(Some(75)),
-    //                     None,
-    //                     true)?;
+    // now that we've located the rows of the Gainers and Losers, we will
+    // discard the first row because it is the table header, and then we'll
+    // extract the ticker info with positive price changes
 
-    /// Take a screenshot of just the WebKit-Infobox
-    // let _png_data = tab
-    //     .wait_for_element("#mw-content-text > div > table.infobox.vevent")?
-    //     .capture_screenshot(ScreenshotFormat::PNG)?;
+    for row in rows.into_iter() {
+        let maybe_header = row.find(|n| n.node_value == "Gainers & Losers");
+        if maybe_header.is_some() {
+            // this is the header of the table, so we skip it because
+            // it doesn't contain intersting data
+            continue;
+        }
+
+        // get the symbol
+        println!("{:?}", row.find(|n| n.attributes.unwrap_or_default()
+            .get("class") == Some(&"wsod_firstCol".to_string())));
+    }
+
     Ok(())
 }
 
