e69793ea54c536222dc70e7bdfdf354eb04914e9
Melvillian <melvillevt@gmail.com>
2019-11-01 17:46:15 -0400

    reduced the number of unwraps in order to make reading errors much easier

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use core::num::ParseFloatError;
+use headless_chrome::protocol::dom::Node;
 use headless_chrome::Browser;
 use std::time::Instant;
 
+/// AfterMarketPriceData holds all the data necessary to track the performance
+/// of a after-market-traded stock over time
 #[derive(Debug)]
 pub struct AfterMarketPriceData {
     symbol: String,
@@ -8,7 +12,39 @@ pub struct AfterMarketPriceData {
     date: Instant,
 }
 
-fn scrape_cnn_after_market_datasource() -> Result<Vec<AfterMarketPriceData>, failure::Error> {
+/// Given a Node, search through its HTML looking for another Node with a tag
+/// whose type is equal to `s`
+fn get_node_with_name<'a>(node: &'a Node, s: &str) -> &'a Node {
+    node.find(|n| n.node_name == s)
+        .unwrap_or_else(|| panic!("couldn't find {:?} tag with node: {:?}", s, node))
+}
+
+/// Same as `get_node_with_class_as_option` but unwraps the Option and panics
+/// if it is `None`
+fn get_node_with_class<'a>(node: &'a Node, s: &str) -> &'a Node {
+    match get_node_with_class_as_option(node, s) {
+        Some(n) => n,
+        _ => panic!("couldn't find {:?}: {:?}", s, node),
+    }
+}
+
+/// Given a Node, search through its HTML looking for another Node with a tag
+/// whose class is equal to 's'
+fn get_node_with_class_as_option<'a>(node: &'a Node, s: &str) -> Option<&'a Node> {
+    node.find(|n| {
+        let attrs = n.attributes.clone(); // TODO learn why compiler won't let me because
+                                          // n.attributes so I don't have to use the slower `.clone`
+        attrs.unwrap_or_default().get("class") == Some(&s.to_string())
+    })
+}
+
+/// Strip away the % char so "+7.06%": String becomes 7.06: f64
+fn parse_percentage_str(mut price_change: String) -> Result<f64, ParseFloatError> {
+    price_change.remove(price_change.len() - 1);
+    price_change.parse::<f64>()
+}
+
+pub fn scrape_cnn_after_market_datasource() -> Result<Vec<AfterMarketPriceData>, failure::Error> {
     let mut after_market_data = Vec::new();
 
     let browser = Browser::default()?;
@@ -19,19 +55,15 @@ fn scrape_cnn_after_market_datasource() -> Result<Vec<AfterMarketPriceData>, fai
     tab.navigate_to("https://money.cnn.com/data/afterhours/")?;
 
     // locate the HTML table with the afterhours trading Gainers and Losers
-    tab.wait_for_element("div#wsod_marketMoversContainer")?;
+    let price_changes_table = tab.wait_for_element("div#wsod_marketMoversContainer")?;
 
-    let table_price_changes = tab.find_element("div#wsod_marketMoversContainer")?;
-
-    let node = table_price_changes.get_description()?;
-    let table = node.find(|n| n.node_name == "TBODY").unwrap();
-
+    let node = price_changes_table.get_description()?;
+    let table = get_node_with_name(&node, "TBODY");
     let rows = table.children.as_ref().unwrap();
 
     // now that we've located the rows of the Gainers and Losers, we will
     // discard the first row because it is the table header, and then we'll
     // extract the ticker info with positive price changes
-
     for row in rows.iter() {
         let maybe_header = row.find(|n| n.node_value == "Gainers & Losers");
         if maybe_header.is_some() {
@@ -39,98 +71,66 @@ fn scrape_cnn_after_market_datasource() -> Result<Vec<AfterMarketPriceData>, fai
             // it doesn't contain intersting data
             continue;
         }
-
         // find the column containing the ticker symbol
-        let first_column = row.find(|n| {
-            let attrs = n.attributes.clone(); // TODO learn why compiler won't let me because
-            // n.attributes so I don't have to use the slower `.clone`
-            attrs.unwrap_or_default().get("class") == Some(&"wsod_firstCol".to_string())
-        }).unwrap();
+        let first_column = get_node_with_class(row, "wsod_firstCol");
 
-        let ticker_symbol: String = first_column
-            .find(|n| n.node_name == "#text").unwrap().node_value.to_string();
+        let ticker_symbol = get_node_with_name(first_column, "#text")
+            .node_value
+            .to_string();
 
         // first, make sure this is a positive price change. We do not want
         // to scrape tickers with negative price changes (cuz we don't think those will make money!)
-        let neg_price_column = row.find(|n| {
-            let attrs = n.attributes.clone(); // TODO learn why compiler won't let me because
-            // n.attributes so I don't have to use the slower `.clone`
-            attrs.unwrap_or_default().get("class") == Some(&"negChangePct".to_string())
-        });
-
+        let neg_price_column = get_node_with_class_as_option(row, "negChangePct");
         if neg_price_column.is_some() {
             break; // ignore this row
         }
 
-        let third_column = row.find(|n| {
-            let attrs = n.attributes.clone(); // TODO learn why compiler won't let me because
-            // n.attributes so I don't have to use the slower `.clone`
-            attrs.unwrap_or_default().get("class") == Some(&"posChangePct".to_string())
-        }).unwrap();
+        let third_column = get_node_with_class(row, "posChangePct");
 
         // this gives us a String of the form "+7.06%"
-        let mut price_perc_change: String = third_column
-            .find(|n| n.node_name == "#text").unwrap().node_value.to_string();
+        let price_perc_change = get_node_with_name(third_column, "#text")
+            .node_value
+            .to_string();
 
-        // strip away the +/- and % chars
-        price_perc_change.remove(price_perc_change.len() - 1);
-        price_perc_change.remove(0);
-
-        let price_perc_change: f64 = price_perc_change.parse()?;
-
+        let price_perc_change = parse_percentage_str(price_perc_change)?;
         let now = Instant::now();
 
         let price_data = AfterMarketPriceData {
             symbol: ticker_symbol,
             percentage: price_perc_change,
-            date: now
+            date: now,
         };
-
         after_market_data.push(price_data);
     }
 
     // we also want the S&P price change, because our strategy takes the movement
     // of the S&P 500 into account (if it's largely positive, then we believe the
     // market will have greater liklihood to buy the trending aftermarket trades)
-
     let standard_poors_price_change = tab.find_element("div#premkContent1")?;
     let node = standard_poors_price_change.get_description()?;
 
-    let sp_row = node.find(|n| {
-        let attrs = n.attributes.clone(); // TODO learn why compiler won't let me because
-        // n.attributes so I don't have to use the slower `.clone`
-        attrs.unwrap_or_default().get("class") == Some(&"wsod_futureQuote wsod_futureQuoteFirst".to_string())
-    }).unwrap();
-
-    let sp_price_changes = sp_row.find(|n| {
-        let attrs = n.attributes.clone(); // TODO learn why compiler won't let me because
-        // n.attributes so I don't have to use the slower `.clone`
-        attrs.unwrap_or_default().get("class") == Some(&"wsod_bold wsod_aRight".to_string())
-    }).unwrap();
+    let sp_row = get_node_with_class(&node, "wsod_futureQuote wsod_futureQuoteFirst");
+    let sp_price_changes = get_node_with_class(sp_row, "wsod_bold wsod_aRight");
 
     // this will get us a String of the form "-0.71%"
-    let mut sp_perc_change = sp_price_changes.find(|n| {
-        n.node_value.contains("%")
-    }).unwrap().node_value.clone();
+    let sp_perc_change = sp_price_changes
+        .find(|n| n.node_value.contains("%"))
+        .unwrap()
+        .node_value
+        .clone(); // TODO firgure out how not to be lazy and not clone everything
 
-    // strip away the % char
-    sp_perc_change.remove(sp_perc_change.len() - 1);
-
-    let sp_perc_change: f64 = sp_perc_change.parse()?;
+    let sp_perc_change = parse_percentage_str(sp_perc_change)?;
 
     let now = Instant::now();
-
     let price_data = AfterMarketPriceData {
         symbol: "S&P".to_string(),
         percentage: sp_perc_change,
-        date: now
+        date: now,
     };
-
     after_market_data.push(price_data);
 
     Ok(after_market_data)
 }
-
 
 fn main() {
     println!("{:?}", scrape_cnn_after_market_datasource().unwrap());
